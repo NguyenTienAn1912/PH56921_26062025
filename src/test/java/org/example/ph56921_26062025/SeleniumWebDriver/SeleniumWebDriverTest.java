0813fee357fdf1ab60b4acda01cfc12823cd70ec
NguyenTienAn1912 <sherlockh050@gmail.com>
2025-06-26 09:57:35 +0700

    Updated

--- src/test/java/org/example/ph56921_26062025/SeleniumWebDriver/SeleniumWebDriverTest.java
+++ src/test/java/org/example/ph56921_26062025/SeleniumWebDriver/SeleniumWebDriverTest.java
@@ -8,28 +8,28 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumWebDriverTest {
-    private WebDriver webDriver;
-
-    @BeforeEach
-    void setUp(){
-        WebDriverManager.chromedriver();
-        webDriver = new ChromeDriver();
-    }
-
-    @Test
-    void testSteps() throws InterruptedException {
-        webDriver.get("https://anupdamoda.github.io/AceOnlineShoePortal/index.html");
-        webDriver.findElement(By.xpath("//*[@id=\"menuToggle\"]/input")).click();
-
-        Thread.sleep(2000);
-        webDriver.findElement(By.xpath("//*[@id=\"menu\"]/a[2]/li")).click();
-
-        Thread.sleep(2000);
-        webDriver.findElement(By.xpath("//*[@id=\"usr\"]")).sendKeys("sa");
-        webDriver.findElement(By.xpath("//*[@id=\"pwd\"]")).sendKeys("sa");
-        webDriver.findElement(By.xpath("//*[@id=\"second_form\"]/input")).click();
-
-        Thread.sleep(2000);
-        webDriver.quit();
-    }
+//    private WebDriver webDriver;
+//
+//    @BeforeEach
+//    void setUp(){
+//        WebDriverManager.chromedriver();
+//        webDriver = new ChromeDriver();
+//    }
+//
+//    @Test
+//    void testSteps() throws InterruptedException {
+//        webDriver.get("https://anupdamoda.github.io/AceOnlineShoePortal/index.html");
+//        webDriver.findElement(By.xpath("//*[@id=\"menuToggle\"]/input")).click();
+//
+//        Thread.sleep(2000);
+//        webDriver.findElement(By.xpath("//*[@id=\"menu\"]/a[2]/li")).click();
+//
+//        Thread.sleep(2000);
+//        webDriver.findElement(By.xpath("//*[@id=\"usr\"]")).sendKeys("sa");
+//        webDriver.findElement(By.xpath("//*[@id=\"pwd\"]")).sendKeys("sa");
+//        webDriver.findElement(By.xpath("//*[@id=\"second_form\"]/input")).click();
+//
+//        Thread.sleep(2000);
+//        webDriver.quit();
+//    }
 }
